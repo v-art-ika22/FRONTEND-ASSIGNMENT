7b995052ca7105a2ce5c340c3f754f60ff6c7e83
v-art-ika22 <vartikaagnihotri9402@gmail.com>
2025-11-19 02:00:12 +0530

    do

--- TASK-4/index.js
+++ TASK-4/index.js
@@ -1,11 +1,47 @@
 
 const bt= document.getElementsByTagName("button")[0];
+const namee= document.getElementById("name");
+const email = document.getElementById("email");
+const alert3 = document.getElementById("emailAlert");
+const passs = document.getElementById("Password");
+ const finall = document.getElementById("submit");
+  const alert2 = document.getElementById("passAlert");
+  namee.addEventListener("input",function(){
+    if(namee.value.length>0){
+       finall.textContent="";
+    }
+  });
+email.addEventListener("input",function(){
+    if(!email.value.includes('@')){
+         alert3.textContent="email must have '@'";
+    }
+    else{
+        alert3.textContent="";
+    }
+});
+passs.addEventListener("input",function(){
+    if(passs.value.length<8){
+        alert2.textContent="Password must contain atleast 8 characters";
+    }
+    else{
+         alert2.textContent="";
+    }
+});
 bt.addEventListener("click",function(){
     const namee= document.getElementById("name");
     const email = document.getElementById("email");
     const passs = document.getElementById("Password");
-    const finall = document.getElementById("submit");
-    if(namee.value===" "|| email.value===" "||passs.value===" "){
+  
+    const alertt1 = document.getElementById("nameAlert");
+    const alert2 = document.getElementById("passAlert");
+    if(namee.value.trim()===""|| email.value.trim()===""||passs.value.trim()===""){
            finall.textContent="Please fill all the fields";
+           alert3.textContent="";
+           alert2.textContent="";
     }
+    else{
+        finall.textContent="";
+    }
+   
+    
 });
